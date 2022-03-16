c42a4df40a34e9ec66df9fb0798447cd566ab591
SlavyanaR <95542891+SlavyanaR@users.noreply.github.com>
2022-03-16 10:39:22 +0300

    Push to popup

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,18 @@
 const profileEditBtn = document.querySelector('.profile__edit-button');
+const popupOpen = document.querySelector('.popup_opened');
+const popupBtnClose = document.querySelector('.popup__button_type_close');
+
+/*открытие попап*/
+function popupOpened(popup) {
+    popup.classList.add('popup_opened');
+}
+
+/*закрытие попап*/ 
+function popupClose(popup) {
+    popup.classList.remove('popup_opened');
+}
+
+profileEditBtn.addEventListener('click', function(){popupOpened(popupOpen)});
+popupBtnClose.addEventListener('click', function(){popupClose(popupOpen)});
+/*https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener*/
+/*https://developer.mozilla.org/ru/docs/Web/API/EventTarget/addEventListener*/
