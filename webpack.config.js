67af934ebbaedb8fd6f8466bce87a00509152193
SlavyanaR <95542891+SlavyanaR@users.noreply.github.com>
2022-05-29 22:56:12 +0300

    final

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin'); 
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
 
 module.exports = {
     entry: { main: './src/index.js' },
@@ -11,10 +12,9 @@ module.exports = {
     },
     mode: 'development',
     devServer: {
-        contentBase: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
+        static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
         compress: true, // это ускорит загрузку в режиме разработки
         port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
-
         open: true // сайт будет открываться сам при запуске npm run dev
     },
     module: {
@@ -25,10 +25,19 @@ module.exports = {
                 exclude: '/node_modules/'// исключает папку node_modules, файлы в ней обрабатывать не нужно
             },
             {
-                // регулярное выражение, которое ищет все файлы с такими расширениями
-                test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+                test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,// регулярное выражение, которое ищет все файлы с такими расширениями
                 type: 'asset/resource'
               },
+              {
+                test: /\.css$/,// применять это правило только к CSS-файлам
+                // при обработке этих файлов нужно использовать
+                // MiniCssExtractPlugin.loader и css-loader
+                use: [MiniCssExtractPlugin.loader, {
+                  loader: 'css-loader',
+                  options: { importLoaders: 1 } // добавьте объект options
+                },
+                'postcss-loader']
+              }
         ]
     },
     plugins: [
@@ -36,5 +45,6 @@ module.exports = {
             template: './src/index.html'
         }),
         new CleanWebpackPlugin(), 
+        new MiniCssExtractPlugin() 
     ]
 }
